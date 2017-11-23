0d1870155144c0f2fa607cbdfe75c155cb390c1a
Dmitry Savostyanov <ds@gosu.ai>
2017-11-23 16:13:42 +0300

    DATASET: logs parser is added

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -7,31 +7,7 @@ import numpy as np
 import pandas as pd
 
 
-def download_google_spreadsheet(url):
-    try:
-        resp = requests.get(url)
-        resp.raise_for_status()
-        content_bytes = resp.content
-    except requests.exceptions.HTTPError as err:
-        print(err, file=sys.stderr)
-    try:
-        content = pd.read_csv(io.BytesIO(content_bytes))
-        return content
-    except Exception as err:
-        print("failed to convert bytes to csv:", err, file=sys.stderr)
-
-
-def query_opendota(sql):
-    resp = requests.get('https://api.opendota.com/api/explorer', params={'sql': sql})
-    data = resp.json()
-    if resp.status_code == 400 and data is not None:
-        sys.stderr.write(data['err'])
-    resp.raise_for_status()
-    return pd.DataFrame.from_records(data['rows'])
-
-
-def query_table():
-    df_table = query_opendota('''
+QUERY = '''
 SELECT
     -- Basic table
     matches.match_id,
@@ -63,35 +39,35 @@ SELECT
     matches.first_blood_time,
 
     --timeStamp variables
-    player_matches.gold_t[3] t_Gold_cnt_3,
-    player_matches.gold_t[5] t_Gold_cnt_5,
-    player_matches.gold_t[8] t_Gold_cnt_8,
-    player_matches.gold_t[10] t_Gold_cnt_10,
-    player_matches.gold_t[12] t_Gold_cnt_12,
-    player_matches.gold_t[15] t_Gold_cnt_15,
-    player_matches.gold_t[20] t_Gold_cnt_20,
-    player_matches.gold_t[25] t_Gold_cnt_25,
-    player_matches.gold_t[30] t_Gold_cnt_30,
+    player_matches.gold_t[3] t_gold_cnt_3,
+    player_matches.gold_t[5] t_gold_cnt_5,
+    player_matches.gold_t[8] t_gold_cnt_8,
+    player_matches.gold_t[10] t_gold_cnt_10,
+    player_matches.gold_t[12] t_gold_cnt_12,
+    player_matches.gold_t[15] t_gold_cnt_15,
+    player_matches.gold_t[20] t_gold_cnt_20,
+    player_matches.gold_t[25] t_gold_cnt_25,
+    player_matches.gold_t[30] t_gold_cnt_30,
 
-    player_matches.lh_t[3] t_LastHits_cnt_3,
-    player_matches.lh_t[5] t_LastHits_cnt_5,
-    player_matches.lh_t[8] t_LastHits_cnt_8,
-    player_matches.lh_t[10] t_LastHits_cnt_10,
-    player_matches.lh_t[12] t_LastHits_cnt_12,
-    player_matches.lh_t[15] t_LastHits_cnt_15,
-    player_matches.lh_t[20] t_LastHits_cnt_20,
-    player_matches.lh_t[25] t_LastHits_cnt_25,
-    player_matches.lh_t[30] t_LastHits_cnt_30,
+    player_matches.lh_t[3] t_lasthits_cnt_3,
+    player_matches.lh_t[5] t_lasthits_cnt_5,
+    player_matches.lh_t[8] t_lasthits_cnt_8,
+    player_matches.lh_t[10] t_lasthits_cnt_10,
+    player_matches.lh_t[12] t_lasthits_cnt_12,
+    player_matches.lh_t[15] t_lasthits_cnt_15,
+    player_matches.lh_t[20] t_lasthits_cnt_20,
+    player_matches.lh_t[25] t_lasthits_cnt_25,
+    player_matches.lh_t[30] t_lasthits_cnt_30,
 
-    player_matches.xp_t[3] t_Experience_cnt_3,
-    player_matches.xp_t[5] t_Experience_cnt_5,
-    player_matches.xp_t[8] t_Experience_cnt_8,
-    player_matches.xp_t[10] t_Experience_cnt_10,
-    player_matches.xp_t[12] t_Experience_cnt_12,
-    player_matches.xp_t[15] t_Experience_cnt_15,
-    player_matches.xp_t[20] t_Experience_cnt_20,
-    player_matches.xp_t[25] t_Experience_cnt_25,
-    player_matches.xp_t[30] t_Experience_cnt_30,
+    player_matches.xp_t[3] t_experience_cnt_3,
+    player_matches.xp_t[5] t_experience_cnt_5,
+    player_matches.xp_t[8] t_experience_cnt_8,
+    player_matches.xp_t[10] t_experience_cnt_10,
+    player_matches.xp_t[12] t_experience_cnt_12,
+    player_matches.xp_t[15] t_experience_cnt_15,
+    player_matches.xp_t[20] t_experience_cnt_20,
+    player_matches.xp_t[25] t_experience_cnt_25,
+    player_matches.xp_t[30] t_experience_cnt_30,
 
     player_matches.pings ping_log,
     player_matches.obs_log,
@@ -99,6 +75,11 @@ SELECT
     player_matches.runes_log,
     player_matches.kills_log,
     player_matches.buyback_log,
+    
+    player_matches.gold_reasons,
+    player_matches.xp_reasons,
+    player_matches.kill_streaks,
+
 
     --table_heroStats
     heroes.localized_name,
@@ -125,8 +106,30 @@ WHERE TRUE
 ORDER BY 
     matches.match_id DESC NULLS LAST
     LIMIT 10000
-    '''.format(**locals()))
-    return df_table
+'''
+
+
+def download_google_spreadsheet(url):
+    try:
+        resp = requests.get(url)
+        resp.raise_for_status()
+        content_bytes = resp.content
+    except requests.exceptions.HTTPError as err:
+        print(err, file=sys.stderr)
+    try:
+        content = pd.read_csv(io.BytesIO(content_bytes))
+        return content
+    except Exception as err:
+        print("failed to convert bytes to csv:", err, file=sys.stderr)
+
+
+def query_opendota(sql):
+    resp = requests.get('https://api.opendota.com/api/explorer', params={'sql': sql})
+    data = resp.json()
+    if resp.status_code == 400 and data is not None:
+        sys.stderr.write(data['err'])
+    resp.raise_for_status()
+    return pd.DataFrame.from_records(data['rows'])
 
 
 def create_unique_id(df_matches):
@@ -200,12 +203,56 @@ def create_first_kill_timing(df_matches):
     return t_first_kills
 
 
-def create_observers_log(df_matches):
-    
+# log = [{'time': 1}, {'time': 2}, {'time': 6}] -->
+# --> aggregations = {'t_<log_name>_5_cnt': 2, 't_<log_name>_10_cnt': 1, ...}
+def aggregate_by_times(log, log_name, times=None):
+    aggregations = {}
+    if times is None:
+        max_min = 31
+        period = 5
+        times = [t for t in range(max_min) if t % period == 0]
+    for agg_type in ('cnt', 'rpm'):
+        aggregations.update({
+            't_{}_{}_{}'.format(log_name, t, agg_type): 0
+            for t in times
+        })
+        for record in log:
+            for t in times:
+                if record['time'] <= t * 60:
+                    agg_name = 't_{}_{}_{}'.format(log_name, t, agg_type)
+                    if agg_type == 'cnt':
+                        aggregations[agg_name] += 1
+                    elif agg_type == 'rpm':
+                        aggregations[agg_name] += 1 / float(t if t != 0 else 1)
+    return aggregations
+
+
+def create_aggregations_from_logs(df_matches):
+    df_observers = df_matches['obs_log'].apply(lambda log: aggregate_by_times(log, 'obs')).apply(pd.Series)
+    df_sentries = df_matches['sen_log'].apply(lambda log: aggregate_by_times(log, 'sen')).apply(pd.Series)
+    df_runes = df_matches['runes_log'].apply(lambda log: aggregate_by_times(log, 'runes')).apply(pd.Series)
+    df_buyback = df_matches['buyback_log'].apply(lambda log: aggregate_by_times(log, 'buybacks'))
+    df_kills = df_matches['kills_log'].apply(lambda log: aggregate_by_times(log, 'kills')).apply(pd.Series)
+    df_gold_reasons = df_matches['gold_reasons'].apply(pd.Series)
+    for col in df_gold_reasons.columns:
+        df_gold_reasons.rename(columns={col: '{}_{}'.format('gold_reason', col)}, inplace=True)
+    df_xp_reasons = df_matches['xp_reasons'].apply(pd.Series)
+    for col in df_xp_reasons.columns:
+        df_xp_reasons.rename(columns={col: '{}_{}'.format('xp_reason', col)}, inplace=True)
+    df_kill_streaks = df_matches['kill_streaks'].apply(pd.Series)
+    for col in df_kill_streaks.columns:
+        df_kill_streaks.rename(columns={col: '{}_{}'.format('kill_streaks', col)}, inplace=True)
+    #TODO: add the rest jsons
+    df_matches = pd.concat([
+        df_matches, df_observers, df_sentries,
+        df_runes, df_buyback, df_kills,
+        df_gold_reasons, df_xp_reasons
+    ], axis=1)
+    return df_matches
 
 
 def create_dataset():
-    df_matches = query_table()
+    df_matches = query_opendota(QUERY)
     df_matches['id'] = create_unique_id(df_matches)
     df_matches['datetime'] = df_matches["start_time"].apply(
         lambda x:
@@ -213,6 +260,12 @@ def create_dataset():
     items = create_core_items_timings(df_matches)
     df_matches = df_matches.merge(items, left_on='id', right_on='id', how='inner')
     df_matches['t_first_kill'] = create_first_kill_timing(df_matches)
+    df_matches = create_aggregations_from_logs(df_matches)
+
+
+    print(df_matches.head())
+    for col in list(df_matches):
+        print(col)
 
 
 if __name__ == '__main__':
