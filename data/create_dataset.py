2d1716d08210de93a014ba17690cd9a9cea73231
Dmitry Savostyanov <ds@gosu.ai>
2017-11-24 18:24:48 +0300

    Dataset: new features from logs and heroes descriptions

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -34,8 +34,15 @@ SELECT
     player_matches.last_hits,
     player_matches.denies,
     player_matches.level,
+    player_matches.rune_pickups,
+    player_matches.lane,
+    player_matches.lane_role,
+    player_matches.is_roaming,
+    player_matches.teamfight_participation,
+    player_matches.towers_killed,
+    player_matches.roshans_killed,
+    player_matches.observers_placed,
     matches.duration,
-    matches.cluster,
     matches.first_blood_time,
 
     --timeStamp variables
@@ -79,6 +86,7 @@ SELECT
     player_matches.gold_reasons,
     player_matches.xp_reasons,
     player_matches.kill_streaks,
+    player_matches.multi_kills,
 
 
     --table_heroStats
@@ -127,7 +135,7 @@ def query_opendota(sql):
     resp = requests.get('https://api.opendota.com/api/explorer', params={'sql': sql})
     data = resp.json()
     if resp.status_code == 400 and data is not None:
-        sys.stderr.write(data['err'])
+        sys.stderr.write(data.get('error'))
     resp.raise_for_status()
     return pd.DataFrame.from_records(data['rows'])
 
@@ -203,14 +211,15 @@ def create_first_kill_timing(df_matches):
     return t_first_kills
 
 
+# Calculates the number of events in log in fixed time range
 # log = [{'time': 1}, {'time': 2}, {'time': 6}] -->
 # --> aggregations = {'t_<log_name>_5_cnt': 2, 't_<log_name>_10_cnt': 1, ...}
 def aggregate_by_times(log, log_name, times=None):
     aggregations = {}
     if times is None:
         max_min = 31
-        period = 5
-        times = [t for t in range(max_min) if t % period == 0]
+        period = 3
+        times = list(range(0, max_min, period))
     for agg_type in ('cnt', 'rpm'):
         aggregations.update({
             't_{}_{}_{}'.format(log_name, t, agg_type): 0
@@ -227,6 +236,7 @@ def aggregate_by_times(log, log_name, times=None):
     return aggregations
 
 
+# From column of dicts the function creates columns with dict keys as names
 def create_aggregations_from_logs(df_matches):
     df_observers = df_matches['obs_log'].apply(lambda log: aggregate_by_times(log, 'obs')).apply(pd.Series)
     df_sentries = df_matches['sen_log'].apply(lambda log: aggregate_by_times(log, 'sen')).apply(pd.Series)
@@ -234,6 +244,10 @@ def create_aggregations_from_logs(df_matches):
     df_buyback = df_matches['buyback_log'].apply(lambda log: aggregate_by_times(log, 'buybacks'))
     df_kills = df_matches['kills_log'].apply(lambda log: aggregate_by_times(log, 'kills')).apply(pd.Series)
     df_gold_reasons = df_matches['gold_reasons'].apply(pd.Series)
+    df_hero_roles = df_matches['role_log'].apply(
+        lambda roles:
+        {'hero_role_{}'.format(role.lower()): 1 for role in roles}
+    ).apply(pd.Series)
     for col in df_gold_reasons.columns:
         df_gold_reasons.rename(columns={col: '{}_{}'.format('gold_reason', col)}, inplace=True)
     df_xp_reasons = df_matches['xp_reasons'].apply(pd.Series)
@@ -241,14 +255,46 @@ def create_aggregations_from_logs(df_matches):
         df_xp_reasons.rename(columns={col: '{}_{}'.format('xp_reason', col)}, inplace=True)
     df_kill_streaks = df_matches['kill_streaks'].apply(pd.Series)
     for col in df_kill_streaks.columns:
-        df_kill_streaks.rename(columns={col: '{}_{}'.format('kill_streaks', col)}, inplace=True)
+        # We should skip kill streaks longer then 11 because of rare values
+        if int(col) > 11:
+            df_kill_streaks.drop(col, axis=1, inplace=True)
+        else:
+            df_kill_streaks.rename(columns={col: '{}_{}'.format('kill_streak', col)}, inplace=True)
+    df_multi_kills = df_matches['multi_kills'].apply(pd.Series)
+    for col in df_multi_kills.columns:
+        df_multi_kills.rename(columns={col: '{}_{}'.format('multi_kill', col)}, inplace=True)
+
+    df_matches['pings'] = df_matches['ping_log'].apply(lambda dct: dct.get('0'))
     #TODO: add the rest jsons
     df_matches = pd.concat([
         df_matches, df_observers, df_sentries,
         df_runes, df_buyback, df_kills,
-        df_gold_reasons, df_xp_reasons
+        df_gold_reasons, df_xp_reasons, df_kill_streaks,
+        df_multi_kills, df_hero_roles,
     ], axis=1)
+    df_matches.drop([
+        'obs_log', 'sen_log',
+        'runes_log', 'buyback_log', 'kills_log',
+        'gold_reasons', 'xp_reasons', 'kill_streaks',
+        'multi_kills', 'ping_log', 'role_log'
+    ], axis=1, inplace=True)
     return df_matches
+
+
+def create_hero_stats_table():
+    hero_stats_url = 'https://api.opendota.com/api/heroStats'
+    resp = requests.get(hero_stats_url)
+    data = resp.json()
+    if resp.status_code == 400 and data is not None:
+        sys.stderr.write(data.get('error'))
+    resp.raise_for_status()
+    df_heroes = pd.DataFrame(data)
+    df_heroes.drop([
+        'id', 'icon', 'img', 'name', 'roles',
+        'attack_type', 'localized_name', 'primary_attr'
+    ], axis=1, inplace=True)
+    df_heroes = df_heroes.fillna(0)
+    return df_heroes
 
 
 def create_dataset():
@@ -258,14 +304,17 @@ def create_dataset():
         lambda x:
         datetime.datetime.fromtimestamp(x).strftime('%d.%m.%Y %H:%M:%S'))
     items = create_core_items_timings(df_matches)
-    df_matches = df_matches.merge(items, left_on='id', right_on='id', how='inner')
+    df_matches = df_matches.merge(items, left_on='id', right_on='id', how='inner', copy=False)
+    df_matches.drop('purchase_log', axis=1, inplace=True)
     df_matches['t_first_kill'] = create_first_kill_timing(df_matches)
     df_matches = create_aggregations_from_logs(df_matches)
+    df_heroes = create_hero_stats_table()
+    df_matches = df_matches.merge(df_heroes, on='hero_id', how='left', suffixes=('', ''), copy=False)
 
-
-    print(df_matches.head())
+    df_matches.head(100).to_csv('dataset.csv')
     for col in list(df_matches):
         print(col)
+    print(df_matches.shape)
 
 
 if __name__ == '__main__':
